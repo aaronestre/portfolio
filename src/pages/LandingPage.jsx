d82397b4a78eeb4a0e1861f89c99aca40b11bab9
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 00:15:04 -0500

    Create Hero layout and CTA Button

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,12 +4,24 @@ import Button from "../components/Button";
 
 export default function LandingPage() {
 
+    const handleMouseEnter = (e) => {
+
+    }
+
+    const handleMouseLeave = (e) => {
+
+    }
+
     return (
         <>
-            <div className="hero">
+            <div className="hero" id="hero">
                 <div className={`hero-container`}>
-                    <h1 className="hero-title">Hello my name's <span className="highlight">Aaron</span></h1>
-                    <h2 className="hero-subtitle">I'm a  <span className="highlight">full stack</span> stack developer and  <span className="highlight">art</span> creative</h2>
+                    <h1 className="hero-title">Hello my name's <span className="swe">Aaron</span></h1>
+                    <h2 className="hero-subtitle">I'm a  <span className="swe">full stack</span> developer and  <span className="art">art</span> creative</h2>
+                </div>
+                <div className="cta-container">
+                    <Button style={{border: "2px solid #FF7043"}} className="swe" text="View My Software Engineering Portfolio" />
+                    <Button style={{border: "2px solid #B39DDB"}} className="art" text="Explore My Art Portfolio" />
                 </div>
             </div>
         </>
