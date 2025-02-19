b620cfa5b031627ad8e222bfc3af39f4f5b0c4b0
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 23:42:36 -0500

    Add hero section and styling to SWE page

--- src/pages/SWE.jsx
+++ src/pages/SWE.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Button from "../components/Button";
+
+import "../styles/SWE.css";
 
 export default function SWE() {
     return (
         <>
-            <h1>SWE</h1>
+            <div className="hero" id="hero">
+                <div className={`hero-container`}>
+                    <h1 className="hero-title">
+                        Hi, I'm Aaron, a <span className="swe">full stack</span> developer
+                    </h1>
+                </div>
+                <div className="cta-container">
+                    <Button
+                        style={{ border: "2px solid #FF7043" }}
+                        className="resume button"
+                        text="Download my Resume"
+                    />
+                    <Button
+                        style={{ border: "2px solid #F9A825" }}
+                        className="github button"
+                        text="Checkout my GitHub"
+                    />
+                </div>
+            </div>
         </>
     );
 }
