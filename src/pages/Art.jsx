bbb160b84c6a223d16e89464cb036f70c2f3fdad
g8t0rboi <aaron.estrellado@gmail.com>
2025-02-18 23:48:10 -0500

    Add hero section and styling for Art page

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Button from "../components/Button";
+
+import "../styles/Art.css";
 
 export default function Art() {
     return (
         <>
-            <h1>Art</h1>
+            <div className="hero" id="hero">
+                <div className={`hero-container`}>
+                    <h1 className="hero-title">
+                        Hi, I'm Aaron, an <span className="art">art</span> creative
+                    </h1>
+                    <h2 className="hero-subtitle">
+                        Explore my <span className="art">art</span> portfolio
+                    </h2>
+                </div>
+            </div>
         </>
     );
 }
