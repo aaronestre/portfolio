1ff6055a6734c755cf79f1dd833e0f77052f9a61
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-05 01:57:52 -0500

    Update ArtImg to use Tailwind for transition

--- src/components/ArtImg.jsx
+++ src/components/ArtImg.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import "../styles/ArtImg.css";
 const ArtImg = ({text, ...props}) => {
 
     return (
-        <div className="art-container relative">
-            <img className="art-image block h-80 w-120 rounded-lg" {...props} />
-            <p className="art-text absolute -translate-x-2/4 -translate-y-2/4 text-[white] text-xl text-center left-2/4 top-2/4">{text}</p>
+        <div className="art-container group relative">
+            <img className="art-image block h-80 w-120 rounded-lg transition duration-300 ease-in-out group-hover:brightness-40" {...props} />
+            <p className="art-text absolute -translate-x-2/4 -translate-y-2/4 text-[white] text-xl text-center left-2/4 top-2/4 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">{text}</p>
         </div>
     )
 }
