b81a0d93c78c326abfea8873f427afd8f5172646
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-04 20:19:52 -0500

    Use css :hover instead of onmouseenter

--- src/components/ArtImg.jsx
+++ src/components/ArtImg.jsx
@@ -2,20 +2,10 @@ import { useState } from "react";
 import "../styles/ArtImg.css";
 const ArtImg = ({text, ...props}) => {
 
-    const [isHovered, setIsHovered] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    }
-
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    }
-
     return (
-        <div className="art-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="art-container">
             <img className="art-image" {...props} />
-            {isHovered && <p className="art-text">{text}</p>}
+            <p className="art-text">{text}</p>
         </div>
     )
 }
