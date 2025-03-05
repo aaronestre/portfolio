bfa2ebbd23ae26a528d56d7216d7aea3cab2787a
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-05 00:39:44 -0500

    Update Hero section to use Tailwind

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({text, className = "", ...props}) {
     return (
         <>
             <button 
-            className={`box-content text-base p-3 rounded-xl w-max shadow-xl ${className}`}
+            className={`box-content text-base p-3 rounded-xl w-max shadow-xl font-semibold ${className}`}
                 {...props}>
                     {text}
             </button>
--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -6,26 +6,26 @@ function Hero() {
     const navigate = useNavigate();
 
     return (
-        <div className="hero" id="hero">
-            <div className={`hero-container`}>
-                <h1 className="hero-title">
-                    Hello, my name's <span className="swe">Aaron</span>.
+        <div className="top-0 left-0 h-dvh flex flex-col justify-center items-center" id="hero">
+            <div className="flex flex-col justify-center items-center text-center mx-auto my-0 px-0 py-4">
+                <h1 className="text-(--primary-font-color) text-6xl m-5">
+                    Hello, my name's <span className="text-(--swe-color)">Aaron</span>.
                 </h1>
-                <h2 className="hero-subtitle">
-                    I'm a <span className="swe">full stack</span> developer and{" "}
-                    <span className="art">art</span> creative.
+                <h2 className="text-(--primary-font-color) text-3xl">
+                    I'm a <span className="text-(--swe-color)">full stack</span> developer and{" "}
+                    <span className="text-(--art-color)">art</span> creative.
                 </h2>
             </div>
-            <div className="cta-container">
+            <div className="m-4 flex justify-center items-center gap-4">
                 <Button
                     style={{ border: "2px solid #FF7043" }}
-                    className="swe"
+                    className="swe text-(--swe-color)"
                     text="View My Software Engineering Portfolio"
                     onClick={() => navigate("/swe")}
                 />
                 <Button
                     style={{ border: "2px solid #B39DDB" }}
-                    className="art"
+                    className="art text-(--art-color)"
                     text="Explore My Art Portfolio"
                     onClick={() => navigate("/art")}
                 />
