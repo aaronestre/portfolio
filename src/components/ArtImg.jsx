7824f90cc9f911c588d9de42da59a2410c31ea15
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-05 20:44:32 -0500

    Update to be responsive

--- src/components/ArtImg.jsx
+++ src/components/ArtImg.jsx
@@ -2,8 +2,8 @@ const ArtImg = ({text, ...props}) => {
 
     return (
         <div className="art-container group relative">
-            <img className="art-image block h-80 w-120 rounded-lg transition duration-300 ease-in-out group-hover:brightness-40" {...props} />
-            <p className="art-text absolute -translate-x-2/4 -translate-y-2/4 text-[white] text-xl text-center left-2/4 top-2/4 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">{text}</p>
+            <img className="block w-full h-auto aspect-[3/2] rounded-lg transition duration-300 ease-in-out group-hover:brightness-40" {...props} />
+            <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-base sm:text-lg md:text-xl text-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">{text}</p>
         </div>
     )
 }
