af51b2e6bd0f1885a4deabedcbb146457aa53394
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-07 02:31:31 -0500

    Add grapch background to Hero

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -15,6 +15,8 @@ function Hero() {
             className="top-0 left-0 h-dvh flex flex-col justify-center items-center px-4 bg-(--background-dark)"
             id="hero"
         >
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:28px_42px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
             <div className="flex flex-col justify-center items-center text-center w-full max-w-xl sm:max-w-2xl mx-auto py-4">
                 <p className="text-white text-sm font-semibold mt-[-5px]">
                     Based in Virginia
