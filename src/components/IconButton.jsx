46671f96fa804ff03a383a3a605586d9f3792fe0
g8t0rboi <aaron.estrellado@gmail.com>
2025-03-07 02:04:00 -0500

    Include buttons that link to socials and resume in Hero

---
+++ src/components/IconButton.jsx
@@ -0,0 +1,25 @@
+import { motion } from "motion/react";
+
+const IconButton = ({ icon, link, ...props }) => {
+    return (
+        <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <motion.button
+                className="rounded-full p-3 bg-transparent border-2 border-white border-solid"
+                whileHover={{
+                    backgroundColor: "#1a1a1a",
+                    scale: 1.15,
+                }}
+                whileTap={{ scale: 0.95 }}
+                {...props}
+            >
+                {icon}
+            </motion.button>
+        </a>
+    );
+};
+
+export default IconButton;
--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -1,33 +1,46 @@
-import { useNavigate } from "react-router";
-import Button from "../Button";
+import IconButton from "../IconButton";
+import {
+    IconBrandGithub,
+    IconBrandLinkedin,
+    IconFileCv,
+} from "@tabler/icons-react";
 
 function Hero() {
-
-    const navigate = useNavigate();
+    const github = <IconBrandGithub color="white" />;
+    const linkedin = <IconBrandLinkedin color="white" />;
+    const resume = <IconFileCv color="white" />;
 
     return (
-        <div className="top-0 left-0 h-dvh flex flex-col justify-center items-center px-4 bg-(--background-dark)" id="hero">
+        <div
+            className="top-0 left-0 h-dvh flex flex-col justify-center items-center px-4 bg-(--background-dark)"
+            id="hero"
+        >
             <div className="flex flex-col justify-center items-center text-center w-full max-w-xl sm:max-w-2xl mx-auto py-4">
-                <h1 className="text-(--primary-font-color) text-4xl sm:text-6xl font-bold leading-tight m-5 whitespace-nowrap max-w-fit">
-                    Hello, my name's <span className="text-(--swe-color)"> Aaron</span>.
+                <p className="text-white text-sm font-semibold mt-[-5px]">
+                    Based in Virginia
+                </p>
+                <h1 className="text-(--primary-font-color) text-4xl sm:text-6xl font-bold leading-tight whitespace-nowrap max-w-fit">
+                    Hello, my name's{" "}
+                    <span className="text-(--swe-color)"> Aaron</span>.
                 </h1>
                 <h2 className="text-(--primary-font-color) text-2xl sm:text-3xl">
-                    I'm a <span className="text-(--swe-color)">full stack</span> developer and{" "}
+                    I'm a <span className="text-(--swe-color)">full stack</span>{" "}
+                    developer and{" "}
                     <span className="text-(--art-color)">art</span> creative.
                 </h2>
             </div>
             <div className="m-4 flex flex-col sm:flex-row justify-center items-center gap-4 w-full px-4">
-                <Button
-                    style={{ border: "2px solid #FF7043" }}
-                    text="View My Software Engineering Portfolio"
-                    type="swe"
-                    onClick={() => navigate("/swe")}
+                <IconButton
+                    icon={github}
+                    link="https://github.com/aaronestre"
                 />
-                <Button
-                    style={{ border: "2px solid #B39DDB" }}
-                    text="Explore My Art Portfolio"
-                    type="art"
-                    onClick={() => navigate("/art")}
+                <IconButton
+                    icon={linkedin}
+                    link="https://www.linkedin.com/in/aaronestrellado/"
+                />
+                <IconButton
+                    icon={resume}
+                    link="/Aaron Estrellado 2025 Resume.pdf"
                 />
             </div>
         </div>
