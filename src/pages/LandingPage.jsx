620bcd961696cbe48ee61dd7c4f17de985c27fa0
aaronestre <aaron.estrellado@gmail.com>
2025-03-19 22:05:39 -0400

    Add button group to toggle between showing SWE and Art

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,29 @@
-import { React } from "react";
+import { React, useState } from "react";
 
 import Hero from "../components/LandingPage/Hero";
 import About from "../components/LandingPage/About";
 import Contact from "../components/LandingPage/Contact";
 import SWESkills from "../components/LandingPage/SWESkills";
 import ArtSkills from "../components/LandingPage/ArtSkills";
+import ButtonGroup from "../components/ButtonGroup";
+
 
 export default function LandingPage() {
+
+    const buttons = [{text: "SWE"} , {text: "Art"}];
+    const [selectedSection, setSelectedSection] = useState("SWE");
+
+    const handleChangeSelection = (selection) => {
+        setSelectedSection(selection);
+    };
+
 
     return (
         <>
             <Hero />
-            <About />
-            <SWESkills />
-            <ArtSkills />
+            <ButtonGroup buttons={buttons} handleChangeSelection={handleChangeSelection} selectedSection={selectedSection}/>
+            <About selectedSection={selectedSection}/>
+            {selectedSection === "SWE" ? <SWESkills /> : <ArtSkills />}
             <Contact />
         </>
     );
