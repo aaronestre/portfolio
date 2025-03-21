f0aec08a2d661a3a9f7eb25eb49cb32d58f54631
aaronestre <aaron.estrellado@gmail.com>
2025-03-20 21:59:12 -0400

    Change alignment of h2 tags

--- src/components/LandingPage/SWESkills.jsx
+++ src/components/LandingPage/SWESkills.jsx
@@ -5,22 +5,28 @@ import Skill from "../Skill";
 
 function SWESkills() {
     const navigate = useNavigate();
-
     const skills = [
         { src: "/images/react.png", alt: "React", text: "React" },
         { src: "/images/java.png", alt: "Java", text: "Java" },
         { src: "/images/csharp.png", alt: "C#", text: "C#" },
     ]
-
     return (
         <div className="flex flex-col justify-center items-center w-11/12 mx-auto my-20 md:my-40" id="swe-overview">
             <h1 className="text-4xl md:text-6xl text-(--swe-color) text-center">Software Engineering</h1>
             <h4 className="text-lg md:text-xl text-center text-(--primary-font-color)">
                 Building scalable, efficient, and user-friendly applications
             </h4>
-            <SkillsContainer skills={skills} />
-            <h2 className="text-3xl md:text-4xl mt-12 mb-6 text-(--primary-font-color)">Featured Project</h2>
-            <div className="flex flex-col lg:flex-row justify-center items-center flex-wrap gap-6 lg:gap-10 h-auto w-full md:w-4/5 mx-auto my-0 p-10 shadow-xl rounded-2xl bg-(--background-semi-dark)">
+            
+            <div className="w-full md:w-2/5 flex flex-col">
+                <h2 className="text-3xl md:text-4xl mt-50 mb-6 text-(--primary-font-color) text-left">Tech Stack</h2>
+                <div className="flex justify-center w-full">
+                    <SkillsContainer skills={skills} />
+                </div>
+                
+                <h2 className="text-3xl md:text-4xl mt-50 mb-6 text-(--primary-font-color) text-left">Featured Project</h2>
+            </div>
+            
+            <div className="flex flex-col lg:flex-row justify-center items-center flex-wrap gap-6 lg:gap-10 h-auto w-2/5 md:w-2/5 mx-auto my-0 p-10 shadow-xl rounded-2xl bg-(--background-semi-dark)">
                 <img
                     className="h-auto w-full sm:w-[80%] md:w-[70%] lg:w-[50%] rounded-xl"
                     src="/images/tagalong.png"
@@ -58,14 +64,17 @@ function SWESkills() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-6 mt-8">
-                <h3 className="text-lg sm:text-xl text-center sm:text-left text-(--primary-font-color)">Want to learn more?</h3>
-                <Button
-                    style={{ border: "2px solid #FF7043" }}
-                    text={"View my other projects"}
-                    className="swe text-(--swe-color)"
-                    onClick={() => navigate("/swe")}
-                />
+            
+            <div className="w-full md:w-3/5">
+                <div className="flex flex-col sm:flex-row justify-center items-center gap-6 mt-8">
+                    <h3 className="text-lg sm:text-xl text-center sm:text-left text-(--primary-font-color)">Want to learn more?</h3>
+                    <Button
+                        style={{ border: "2px solid #FF7043" }}
+                        text={"View my other projects"}
+                        className="swe text-(--swe-color)"
+                        onClick={() => navigate("/swe")}
+                    />
+                </div>
             </div>
         </div>
     );
