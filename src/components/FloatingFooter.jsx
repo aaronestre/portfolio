9a580f4193aec81bc650a236945089cb7b66036b
aaronestre <aaron.estrellado@gmail.com>
2025-03-23 23:50:06 -0400

    Move data for footerlinks into separate file for easier scalability

--- src/components/FloatingFooter.jsx
+++ src/components/FloatingFooter.jsx
@@ -1,16 +1,13 @@
 import FooterLink from "./FooterLink";
-
+import { navigationLinks, socialLinks } from "../util/footerLinksData";
 
 export default function FloatingFooter() {
-    
-    const listItem = "text-(--secondary-font-color) hover:text-white transition duration-300 cursor-pointer"
-
 
 	return (
 		<div className="bg-(--background-semi-dark) p-4 w-1/3 mx-auto grid grid-cols-2 mb-10 rounded-md h-auto">
 			<div className="flex flex-col justify-end">
 				<h1 className="text-(--primary-font-color) font-bold text-5xl">
-					Aaron<br></br>
+					Aaron<br/>
                     Estrellado. 
 				</h1>
 			</div>
@@ -20,11 +17,9 @@ export default function FloatingFooter() {
 						Explore
 					</h1>
 					<div className="flex flex-col">
-
-						<FooterLink href="/" text="Home"/>
-                        <FooterLink href="/about" text="About"/>
-                        <FooterLink href="/swe" text="SWE"/>
-                        <FooterLink href="/art" text="Art"/>
+						{navigationLinks.map((link, index) => (
+							<FooterLink key={index} href={link.href} text={link.text} type={link.type} />
+						))}
 					</div>
 				</div>
 				<div className="h-full flex flex-col justify-start">
@@ -32,9 +27,9 @@ export default function FloatingFooter() {
 						Socials
 					</h1>
 					<div className="flex flex-col">
-						<FooterLink href="https://github.com/aaronestre" text="GitHub" type="external" />
-                        <FooterLink href="https://www.linkedin.com/in/aaronestrellado/" text="LinkedIn" type="external" />
-
+						{socialLinks.map((link, index) => (
+							<FooterLink key={index} href={link.href} text={link.text} type={link.type} />
+						))}
 					</div>
 				</div>
 			</div>
---
+++ src/util/footerLinksData.js
@@ -0,0 +1,12 @@
+export const navigationLinks = [
+    {text: 'Home', href: '/', type: "internal" },
+
+    {text: 'About', href: '/about', type: "internal"},
+    {text: 'SWE', href: '/swe', type: "internal"},
+    {text: 'Art', href: '/art', type: "internal"}
+]
+
+export const socialLinks = [
+    {text: 'LinkedIn', href: 'https://www.linkedin.com/in/aaronestrellado/', type: "external" },
+    {text: 'GitHub', href: 'https://github.com/aaronestre', type: "external" }
+]
